566b4fd40db5b2da0c5c080a53f6cb365c01c90c
kyungpyo.kim <kimkyungpyo@nativebinary.com>
2026-02-24 23:10:03 +0900

    레이어 프리즈 활성화

--- src/lib/skin_disease/model_factory.py
+++ src/lib/skin_disease/model_factory.py
@@ -26,8 +26,9 @@ def get_model_and_preprocess(model_name, num_classes=5):
 
         for param in model.classifier.parameters():
             param.requires_grad = True
-        for param in model.stages[3].parameters():
+        for param in model.features[7].parameters():
             param.requires_grad = True
+
     elif model_name == 'efficientnet_v2_s':
         weights = models.EfficientNet_V2_S_Weights.DEFAULT
         model = models.efficientnet_v2_s(weights=weights)
